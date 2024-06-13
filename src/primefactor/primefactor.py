3579f4f4fc36f1493c26f5aec6e75d0a28ab3cdc
demianmedich <demianmedich@gmail.com>
2024-06-13 09:20:30 +0900

    Initial settings

---
+++ src/primefactor/primefactor.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+
+class PrimeFactor:
+
+    def of(self, n: int) -> list[int]:
+        raise NotImplementedError()
---
+++ tests/unittest/test_primefactor.py
@@ -0,0 +1,31 @@
+import unittest
+
+from src.primefactor.primefactor import PrimeFactor
+
+
+class PrimeFactorTestCase(unittest.TestCase):
+    def test_primefactor_of_1(self):
+        pf = PrimeFactor()
+
+        with self.assertRaises(ValueError):
+            pf.of(1)
+
+    def test_primefactor_of_n(self):
+        pf = PrimeFactor()
+
+        expected_pairs = (
+            (2, [2]),
+            (3, [3]),
+            (4, [2, 2]),
+            (5, [5]),
+            (6, [2, 3]),
+            (7, [7]),
+            (8, [2, 2, 2]),
+        )
+
+        for n, primes in expected_pairs:
+            self.assertEqual(primes, pf.of(n))
+
+
+if __name__ == '__main__':
+    unittest.main()
